31daeb8c98d5751f49303f0e9945bfa943a1435b
LoveAndCode <practice1356@gmail.com>
2021-10-27 00:04:52 +0900

    project init

--- todo-server/src/main/java/todoserver/TodoServerApplication.java
+++ todo-server/src/main/java/todoserver/TodoServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TodoServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TodoServerApplication.class, args);
+	}
 }
 
